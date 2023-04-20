45ed11d9dc9932724feb92c3e096cca3ffc6510d
stepa <gershuni.stepan@gmail.com>
2023-04-20 10:36:37 +0100

    GA

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import Script from 'next/script'
 
 const defaultMeta = {
   title: 'GigaBrain Academy',
@@ -43,49 +44,61 @@ export default function Seo(props: SeoProps) {
   // });
 
   return (
-    <Head>
-      <title>{meta.title}</title>
-      <meta name='robots' content={meta.robots} />
-      <meta content={meta.description} name='description' />
-      <meta property='og:url' content={`${meta.url}${router.asPath}`} />
-      <link rel='canonical' href={`${meta.url}${router.asPath}`} />
-      {/* Open Graph */}
-      <meta property='og:type' content={meta.type} />
-      <meta property='og:site_name' content={meta.siteName} />
-      <meta property='og:description' content={meta.description} />
-      <meta property='og:title' content={meta.title} />
-      <meta name='image' property='og:image' content={meta.image} />
-      {/* Twitter */}
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:site' content='@sgershuni' />
-      <meta name='twitter:title' content={meta.title} />
-      <meta name='twitter:description' content={meta.description} />
-      <meta name='twitter:image' content={meta.image} />
-      {meta.date && (
-        <>
-          <meta property='article:published_time' content={meta.date} />
-          <meta
-            name='publish_date'
-            property='og:publish_date'
-            content={meta.date}
-          />
-          <meta
-            name='author'
-            property='article:author'
-            content='Stepan Gershuni'
-          />
-        </>
-      )}
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name='robots' content={meta.robots} />
+        <meta content={meta.description} name='description' />
+        <meta property='og:url' content={`${meta.url}${router.asPath}`} />
+        <link rel='canonical' href={`${meta.url}${router.asPath}`} />
+        {/* Open Graph */}
+        <meta property='og:type' content={meta.type} />
+        <meta property='og:site_name' content={meta.siteName} />
+        <meta property='og:description' content={meta.description} />
+        <meta property='og:title' content={meta.title} />
+        <meta name='image' property='og:image' content={meta.image} />
+        {/* Twitter */}
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@sgershuni' />
+        <meta name='twitter:title' content={meta.title} />
+        <meta name='twitter:description' content={meta.description} />
+        <meta name='twitter:image' content={meta.image} />
+        {meta.date && (
+          <>
+            <meta property='article:published_time' content={meta.date} />
+            <meta
+              name='publish_date'
+              property='og:publish_date'
+              content={meta.date}
+            />
+            <meta
+              name='author'
+              property='article:author'
+              content='Stepan Gershuni'
+            />
+          </>
+        )}
 
-      {/* Favicons */}
-      {favicons.map((linkProps) => (
-        <link key={linkProps.href} {...linkProps} />
-      ))}
-      <meta name='msapplication-TileColor' content='#ffffff' />
-      <meta name='msapplication-config' content='/favicon/browserconfig.xml' />
-      <meta name='theme-color' content='#ffffff' />
-      <script async src="https://tally.so/widgets/embed.js"></script>
-    </Head>
+        {/* Favicons */}
+        {favicons.map((linkProps) => (
+          <link key={linkProps.href} {...linkProps} />
+        ))}
+        <meta name='msapplication-TileColor' content='#ffffff' />
+        <meta name='msapplication-config' content='/favicon/browserconfig.xml' />
+        <meta name='theme-color' content='#ffffff' />
+      </Head>
+      <Script async src="https://tally.so/widgets/embed.js"></Script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-WCV6D1MT9D" strategy="afterInteractive"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-WCV6D1MT9D');
+        `}
+      </Script>
+    </>
   );
 }
 
